27bc30436c61dab113a398a577bbc2befcbf7d7c
Peter Wagener <mail@peterwagener.net>
2020-05-13 21:17:55 +0200

    operations: updated inclusion list

--- src/haksolid2/operations/__init__.py
+++ src/haksolid2/operations/__init__.py
@@ -1,4 +1,4 @@
 from .base import DAGOperation
 from .boolean import difference, intersection
 from .sums import minkowski, offset
-from .extrusion import rotate_extrude, linear_extrude
+from .extrusion import ExtrusionNode, LinearExtrude, rotate_extrude, linear_extrude

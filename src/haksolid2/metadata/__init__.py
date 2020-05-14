cb8d40b5e2222504afb021851d9292489c65984f
Peter Wagener <mail@peterwagener.net>
2020-05-14 21:21:47 +0200

    implemented dag layers

--- src/haksolid2/metadata/__init__.py
+++ src/haksolid2/metadata/__init__.py
@@ -1,2 +1,3 @@
 from .appearance import color
 from .graphinfo import DimensionVisitor
+from .layers import DAGLayer, previewLayer, nonpreviewLayer, LayerFilter, AllLayerFilter, NoLayerFilter, ClassLayerFilter
---
+++ src/haksolid2/metadata/layers.py
@@ -0,0 +1,42 @@
+from .. import dag
+from ..math import *
+
+
+class DAGLayer(dag.DAGGroup):
+	color = V(1, 1, 1)
+	alpha = 1
+
+	def __init__(s):
+		dag.DAGGroup.__init__(s)
+
+
+class previewLayer(DAGLayer):
+	color = V(0.2, 0.8, 0.5)
+	alpha = 0.4
+
+
+class nonpreviewLayer(DAGLayer):
+	pass
+
+
+class LayerFilter:
+	def __call__(s, layer: DAGLayer):
+		raise NotImplementedError()
+
+
+class AllLayerFilter(LayerFilter):
+	def __call__(s, layer: DAGLayer):
+		return True
+
+
+class NoLayerFilter(LayerFilter):
+	def __call__(s, layer: DAGLayer):
+		return False
+
+
+class ClassLayerFilter(LayerFilter):
+	def __init__(s, *classes):
+		s.classes = set(classes)
+
+	def __call__(s, layer: DAGLayer):
+		return layer.__class__ in s.classes
